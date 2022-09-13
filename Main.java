77eab5e7835380e64916c241850dddea1cd13ee7
HariPurwanto <haripurwanto446@gmail.com>
2022-09-13 14:07:24 +0700

    tambah hari

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello guysys");
         System.out.println("nabila");
-        System.out.println("aku belajar java");
+        System.out.println("aku belajar javla");
+        System.out.println("aku belajar d");
     }
 }
